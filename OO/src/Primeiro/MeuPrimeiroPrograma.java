b5bab00941d6090416453f8c537869d0efa3cb8c
felli <felli@DESKTOP-PPFRNJ7.domain.name>
2021-07-19 15:43:22 -0300

    Meu primeiro Comitt!

---
+++ OO/src/Primeiro/MeuPrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package Primeiro;
+
+public class MeuPrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Meu Primeiro Programa em Java");
+	}
+
+}
